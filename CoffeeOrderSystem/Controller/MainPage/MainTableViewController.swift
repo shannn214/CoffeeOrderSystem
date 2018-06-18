f3104716cb77362060bc252e7a431b1cf718b3d4
Grace <gracejin214@gmail.com>
2018-06-18 14:02:50 +0800

    firebase issue

--- CoffeeOrderSystem/Controller/MainPage/MainTableViewController.swift
+++ CoffeeOrderSystem/Controller/MainPage/MainTableViewController.swift
@@ -15,13 +15,43 @@ class MainTableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupTableView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    private func setupTableView() {
+        
+        let nib = UINib(nibName: String(describing: OrderPageTableViewCell.self), bundle: nil)
+        mainTableView.register(nib, forCellReuseIdentifier: String(describing: OrderPageTableViewCell.self))
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+        mainTableView.rowHeight = 120
+        
+    }
+
+
+}
+
+extension MainTableViewController: UITableViewDelegate, UITableViewDataSource {
     
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let orderCell = mainTableView.dequeueReusableCell(withIdentifier: String(describing: OrderPageTableViewCell.self),
+                                                                for: indexPath) as? OrderPageTableViewCell else { return UITableViewCell() }
+        
+        
+        
+        return orderCell
+    }
+    
+    
+    
+    
 }
