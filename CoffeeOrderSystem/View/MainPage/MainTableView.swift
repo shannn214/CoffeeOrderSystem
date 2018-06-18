d08ef1a38566135a83bdc6fee5e1a3722693132b
Grace <gracejin214@gmail.com>
2018-06-18 11:15:12 +0800

    setup folder and file

---
+++ CoffeeOrderSystem/View/MainPage/MainTableView.swift
@@ -0,0 +1,16 @@
+//
+//  MainTableView.swift
+//  CoffeeOrderSystem
+//
+//  Created by 尚靖 on 2018/6/18.
+//  Copyright © 2018年 尚靖. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTableView: UITableView {
+    
+    
+    
+}
---
+++ CoffeeOrderSystem/View/MainPage/OrderPageCell/OrderPageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderPageTableViewCell.swift
+//  CoffeeOrderSystem
+//
+//  Created by 尚靖 on 2018/6/18.
+//  Copyright © 2018年 尚靖. All rights reserved.
+//
+
+import UIKit
+
+class OrderPageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
